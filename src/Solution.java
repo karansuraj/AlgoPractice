c858c0a66161a945d9b6ad7f12fd49b4ea781fac
Karan Suraj <karansuraj@gmail.com>
2018-08-30 18:44:41 -0400

    Fixing bugs for edge cases and missing the last needed iteration

--- src/Solution.java
+++ src/Solution.java
@@ -1,39 +1,33 @@
 class Solution {
-    public int reverse(int x) {
+    int reverse(int x) {
         /* Count the number of digits and the number of 10s places in the final output */
         if(x/10==0) return x;
         int out=0;
-        int tenCt = 0; //Will be the number of digits in output
-        int numDigits = 0; //Will be the number of digits in input
-        //int x_temp = Math.abs(x); //Using absolute value to make operations easier
-        boolean isNeg = (x<0);
-        x = Math.abs(x);
-        int x_temp = x;
+        int numDigitsOut; //Will be the number of digits in output
+        int numDigitsIn = 0; //Will be the number of digits in input
+        long long_x = (long) Math.abs(x); //Using absolute value to make operations easier
+        long x_temp = long_x;
         while(x_temp>0) {
-            if(x_temp>0) tenCt++; //Number of digits in output could be less than input
-            numDigits++;
+            numDigitsIn++;
             x_temp/=10;
         }
-        int inPow,outPow;
-        //System.out.println(tenCt + " " + numDigits);
-        for(int i=0; i<tenCt; i++){ //Counting number of digits to add to out, up
-            inPow = (int) (Math.pow(10,numDigits-1));
-            outPow = (int) Math.pow(10,i);
-            //System.out.println(inPow + " " + outPow);
+        long inPow,outPow;
+        numDigitsOut = numDigitsIn;
+        for(int i=0; i<numDigitsOut; i++){ //Counting number of digits to add to out, up
+            inPow = (long) (Math.pow(10,numDigitsIn-1)); //Power of 10 of top digit
+            outPow = (long) Math.pow(10,i); //Power of 10 of digit we are adding to output
             //numDigits - 1, because 10 is power of 1 of 10, but is 2 digits
-            x_temp = x/inPow; // Divides to leave only top digit
+            x_temp = long_x/inPow; // Divides to leave only top digit
+            if(out + x_temp*outPow > Integer.MAX_VALUE) return 0; // if the result overflows int type, return 0
             out+=x_temp*outPow; //Add top digit to lower digits of output
-            x = x - x_temp*inPow; //Subtract top digit from input
-            numDigits--;
-            /*System.out.println(x + " " + x_temp + " " + out);
-            System.out.println();*/
+            long_x = long_x - x_temp*inPow; //Subtract top digit from input
+            numDigitsIn--;
         }
-        if(isNeg) out*=-1;
+        if(x<0) out*=-1;
         return out;
-        //char[] nChrs = x.toCharArray();
     }
 
-    public String longestPalindrome(String s) {
+    String longestPalindrome(String s) {
         /* Seems like a dynamic programming problem we can use a 2D array to keep track of records
         T(i,j) = {
                     if(T(i+1, j-1) > 0 && s.charAt(i) == s.charAt(j))

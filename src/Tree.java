e349b9df520ba06d74fb4a7cf6d47abdbbd89e1c
Karan Suraj <karansuraj@gmail.com>
2018-09-19 18:42:22 -0400

    Added function for finding max depth into tree

--- src/Tree.java
+++ src/Tree.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 // Java program to construct binary tree from
 // given array in level order fashion 
-// Taken from geeksforgeeks to help solve LeetCode Tree problems
+// Node class, insertLevelOrder & inOrder functions taken from geeksforgeeks to help solve LeetCode Tree problems
 // https://www.geeksforgeeks.org/construct-complete-binary-tree-given-array/
 public class Tree {
     Node root;
@@ -48,8 +48,20 @@ public class Tree {
             //System.out.print(root.data + " ");
         }
     }
+    // Driver program to test above function
+    public static void main(String args[])
+    {
+        Tree t2 = new Tree();
+        int arr[] = { 1, 2, 3, 4, 5, 6, 6, 6, 6 };
+        t2.root = t2.insertLevelOrder(arr, t2.root, 0);
+        ArrayList<Integer> arr2d2 = new ArrayList<>();
+        t2.inOrder(t2.root, arr2d2);
+        for(Integer i: arr2d2) System.out.print(i+" ");
+    }
 
+    /*Code below is by Karan Suraj*/
     //Recursive function that will search both trees and compare values along the way to check if trees are the same
+    //Call this function initially with "isSame" set to true
     public boolean compareTrees(Node root1, Node root2, boolean isSame){ //false booleans will propagate up the recursion
         if (root1 != null && root2!= null){ //As long as both are not null
             if(root1.data==root2.data && isSame) isSame=true; //Check if values are same at the current node
@@ -61,14 +73,13 @@ public class Tree {
         return isSame;
     }
 
-    // Driver program to test above function 
-    public static void main(String args[])
-    {
-        Tree t2 = new Tree();
-        int arr[] = { 1, 2, 3, 4, 5, 6, 6, 6, 6 };
-        t2.root = t2.insertLevelOrder(arr, t2.root, 0);
-        ArrayList<Integer> arr2d2 = new ArrayList<>();
-        t2.inOrder(t2.root, arr2d2);
-        for(Integer i: arr2d2) System.out.print(i+" ");
+    //Recursive function that'll dive into a tree and find the maximum level depth
+    public int maxDepthDFS(Node root, int count){
+        if(root!=null){
+            count++; //Increment count for current node
+            count = Math.max(maxDepthDFS(root.left, count),maxDepthDFS(root.right, count)); //Choose max count from left or right side
+        }
+        return count;
+
     }
 } 
